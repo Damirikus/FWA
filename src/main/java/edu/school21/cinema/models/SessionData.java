08cf9f588d418147f8fa920002bb28d0cb0678e9
Damirikus <Damir.gazizoff.2011.sta.mail_59@mail.ru>
2023-03-12 13:30:40 +0300

    !

--- src/main/java/edu/school21/cinema/models/SessionData.java
+++ src/main/java/edu/school21/cinema/models/SessionData.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SessionData {
     private Long s_id;
     private Long usr_id;
